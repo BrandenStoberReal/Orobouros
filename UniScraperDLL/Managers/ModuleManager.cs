a9234e7b1c865d9f254babc7e9a211c2a2543906
Branden Stober <67252380+BrandenStoberReal@users.noreply.github.com>
2024-01-28 00:11:03 -0500

    Added module manager

---
+++ UniScraperDLL/Managers/ModuleManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniScraperDLL.Managers
+{
+    public static class ModuleManager
+    {
+        public static List<object> modules = new List<object>();
+
+        /// <summary>
+        /// Ensures the modules folder exists. This acts as a default modules path.
+        /// </summary>
+        public static void VerifyModulesFolderIntegrity()
+        {
+            // Create modules directory if non-existent
+            if (!Directory.Exists("./modules"))
+            {
+                Directory.CreateDirectory("./modules");
+            }
+        }
+
+        /// <summary>
+        /// Loads assemblies. Can optionally provide a custom folder to load modules from.
+        /// </summary>
+        /// <param name="folder"></param>
+        public static void LoadAssemblies(string? folder = null)
+        {
+            foreach (var mod in Directory.GetFiles(folder != null ? folder : "./modules"))
+            {
+                var DLL = Assembly.LoadFile(mod);
+                modules.Add(DLL);
+            }
+        }
+    }
+}
