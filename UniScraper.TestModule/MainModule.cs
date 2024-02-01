25b5ba7dff3603a08fdea0e7c226a27eadf75fe3
Branden Stober <67252380+BrandenStoberReal@users.noreply.github.com>
2024-02-01 04:08:15 -0500

    Misc

--- UniScraper.TestModule/MainModule.cs
+++ UniScraper.TestModule/MainModule.cs
@@ -3,8 +3,12 @@ using static UniScraperDLL.UniAssemblyInfo;
 
 namespace UniScraper.TestModule
 {
+    // Module must inherit ScraperInfo.
     public class MainModule : ScraperInfo
     {
+        // Module information. Must be in the module and names must not be changed. Values can be
+        // freely modified.
+
         public override string Name { get; set; } = "Test Module";
         public override string Description { get; set; } = "A simple test module";
         public override string ModuleVersion { get; set; } = "1.0.0.0";
