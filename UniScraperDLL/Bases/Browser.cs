c5aca3fc695fe09e7a186598fe98cf4d9167b620
Branden Stober <67252380+BrandenStoberReal@users.noreply.github.com>
2024-02-01 00:19:56 -0500

    Selenium implementation

---
+++ UniScraperDLL/Bases/Browser.cs
@@ -0,0 +1,38 @@
+﻿using Selenium.Extensions;
+using Selenium.WebDriver.UndetectedChromeDriver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniScraperDLL.Bases
+{
+    public class Browser
+    {
+        /// <summary>
+        /// Undetected Chrome Driver instance.
+        /// </summary>
+        public SlDriver WebDriver { get; private set; }
+
+        /// <summary>
+        /// Instantiator
+        /// </summary>
+        /// <param name="profileName">Selenium profile name. Defaults to "uniscraper".</param>
+        /// <param name="headless">
+        /// Whether the Selenium instance runs headless. Some websites detect this. Defaults to true.
+        /// </param>
+        public Browser(string profileName = "uniscraper", bool headless = true)
+        {
+            WebDriver = UndetectedChromeDriver.Instance(profileName, Headless: headless);
+        }
+
+        /// <summary>
+        /// Clean up any background instances.
+        /// </summary>
+        public void Dispose()
+        {
+            WebDriver.Quit();
+        }
+    }
+}
---
+++ UniScraperDLLTests/DLL/BrowserTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniScraperDLL.Bases;
+
+namespace UniScraperDLLTests.DLL
+{
+    [TestClass]
+    public class BrowserTests
+    {
+        [TestMethod(displayName: "Browser Test - Fetch")]
+        public async Task Browser_Fetch_Test()
+        {
+            var browser = new Browser();
+            browser.WebDriver.GoTo("https://www.google.com/");
+            Assert.IsTrue(browser.WebDriver.PageSource != "");
+            browser.Dispose();
+        }
+    }
+}
