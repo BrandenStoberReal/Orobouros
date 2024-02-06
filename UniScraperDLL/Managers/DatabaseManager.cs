03c5829da1483b02716713f0cdefa20b0cac61a7
Branden Stober <67252380+BrandenStoberReal@users.noreply.github.com>
2024-02-05 22:11:35 -0500

    Database stuff

---
+++ UniScraperDLL/Managers/DatabaseManager.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniScraperDLL.Managers
+{
+    public static class DatabaseManager
+    {
+        /// <summary>
+        /// Connects to an sqlite database file.
+        /// </summary>
+        /// <param name="databaseFile"></param>
+        /// <returns></returns>
+        public static SqliteConnection ConnectToDatabase(string databaseFile)
+        {
+            SqliteConnection connection = new SqliteConnection($"Data Source={databaseFile}");
+            connection.Open();
+            return connection;
+        }
+
+        /// <summary>
+        /// Closes a connection to an sqlite database file.
+        /// </summary>
+        /// <param name="connection"></param>
+        public static void CloseConnection(SqliteConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
