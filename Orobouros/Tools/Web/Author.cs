2ad79c0c2a8a0c0dffc0e13adf56d42598a723fe
Branden Stober <67252380+BrandenStoberReal@users.noreply.github.com>
2024-02-25 01:07:42 -0500

    Added more web classes

---
+++ Orobouros/Tools/Web/Author.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orobouros.Tools.Web
+{
+    public class Author
+    {
+        public string Username { get; set; }
+        public string URL { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Region { get; set; }
+        public string? Language { get; set; }
+        public Image? ProfilePicture { get; set; }
+        public DateTime? AccountCreationDate { get; set; }
+        public DateTime? LastOnline { get; set; }
+    }
+}
---
+++ Orobouros/Tools/Web/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orobouros.Tools.Web
+{
+    public class Comment
+    {
+        public Post ParentPost { get; set; }
+        public string URL { get; set; }
+        public string Author { get; set; }
+        public DateTime PostTime { get; set; }
+        public DateTime LastEdited { get; set; }
+        public string Content { get; set; }
+        public List<Comment> Replies { get; set; }
+    }
+}
